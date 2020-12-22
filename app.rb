45445d7d3457dc4c5c495b6875f478598dd368fc
Michael Fuget <michaelfuget@gmail.com>
2020-12-22 06:52:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,5 +4,14 @@ class App < Sinatra::Base
   get '/' do
     "Hello, World!"
   end
+  get '/name' do
+    "Hello, World!"
+  end
+  get '/hometown' do
+    "Hello, World!"
+  end
+  get '/favorite-song' do
+    "Hello, World!"
+  end
 
 end
