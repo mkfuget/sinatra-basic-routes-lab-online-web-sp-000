c1bf530e8247316e106f00b1c1772133762cb874
Michael Fuget <michaelfuget@gmail.com>
2020-12-22 06:53:28 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+  get '/name' do
+    "My name is Michael Fuget!"
+  end
+  get '/hometown' do
+    "My hometown is Seattle!"
+  end
+  get '/favorite-song' do
+    "My favorite song is Run Away With Me!"
+  end
+
 end
